93811ffc16dc022e03059d036458bb06d72810e1
anastasia.psitou <anastasia.psitou@gmail.com>
2023-04-27 02:07:35 +0300

    Julia guide notebook

---
+++ notebooks/julia_guide.jl
@@ -0,0 +1,13 @@
+### A Pluto.jl notebook ###
+# v0.14.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 1b6bbd98-e486-11ed-114e-bfcdc6e92054
+html"""
+<h1> <center>JULIA  GUIDE</center> </h1>
+"""
+
+# ╔═╡ Cell order:
+# ╟─1b6bbd98-e486-11ed-114e-bfcdc6e92054
